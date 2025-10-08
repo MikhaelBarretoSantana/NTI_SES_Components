e8b96d04608ecaebc6644e5d4af436ce9db17abd
mikhae.santana <mikhael.santana@sergipetec.org.br>
2025-10-08 11:19:12 -0300

    hotfix: modal import

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -53,7 +53,7 @@ const CONFIRM_ICONS = {
 
 let globalZIndex = 1000;
 
-const Modal = forwardRef<ModalComponentRef, ModalProps>(({
+export const Modal = forwardRef<ModalComponentRef, ModalProps>(({
     // Content
     title,
     children,
@@ -639,5 +639,3 @@ const Modal = forwardRef<ModalComponentRef, ModalProps>(({
 });
 
 Modal.displayName = 'Modal';
-
-export default Modal;
